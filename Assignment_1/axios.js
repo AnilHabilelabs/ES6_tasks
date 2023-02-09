378a0bfa0b909a0d733f4bd3f344661d34fdd0fd
Anil Kumar <anil.kumar@habilelabs.io>
2023-02-09 11:37:23 +0530

    all task files added

---
+++ Assignment_1/axios.js
@@ -0,0 +1,27 @@
+
+const BASE_URL = 'https://jsonplaceholder.typicode.com/posts';
+
+const getData = async () => {
+  try {
+    const response = await axios.get(`${BASE_URL}`);
+
+    let content = document.querySelector(".content");
+
+    console.log(response);
+
+    content.innerHTML = (response.data).map(element =>
+      `
+            <tr>
+                <td>${element['userId']}</td>
+                <td>${element['id']}</td>
+                <td>${element['title']}</td>
+            </tr>
+        `
+    ).join('');
+
+  } catch (errors) {
+    console.error(errors);
+  }
+};
+
+getData();
